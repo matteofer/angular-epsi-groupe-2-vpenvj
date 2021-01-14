a58bfb3cec9f111bc162c7ff8194f82ede7babfa
matteofer <matteo.fereire@gmail.com>
2021-01-14 11:44:26 +0100

    tentative snackbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {CoreModule} from './core/core.module';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
-import {ApiInterceptor} from './core/api.interceptor';
-import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CoreModule } from "./core/core.module";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ApiInterceptor } from "./core/api.interceptor";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSnackBar
+} from "@angular/material";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     CoreModule,
     BrowserModule,
@@ -21,14 +24,15 @@ import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
+    MatSnackBar
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
-      multi: true,
-    },
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
