9f8b510adb417738de4aef190e7b1fc8562ce7b5
matteofer <matteo.fereire@gmail.com>
2021-01-14 10:23:45 +0100

    3 files modified

--- src/app/core/entities/user.ts
+++ src/app/core/entities/user.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:variable-name */
-import {TagInterface} from '../interfaces/tag.interface';
+import { TagInterface } from "../interfaces/tag.interface";
 
 export class User {
   id?: string;
@@ -8,11 +8,12 @@ export class User {
   email?: string;
   password?: string;
   tags?: TagInterface[];
+  roles?: string[];
 
   constructor(obj?: any) {
-    this.first_name = 'first_name' in obj ? obj.first_name : '';
-    this.last_name = 'last_name' in obj ? obj.last_name : '';
-    this.email = 'email' in obj ? obj.email : '';
-    this.password = 'password' in obj ? obj.password : '';
+    this.first_name = "first_name" in obj ? obj.first_name : "";
+    this.last_name = "last_name" in obj ? obj.last_name : "";
+    this.email = "email" in obj ? obj.email : "";
+    this.password = "password" in obj ? obj.password : "";
   }
 }
